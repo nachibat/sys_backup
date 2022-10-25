cd73671d504cdcf71f08eabd4e35bea8e6cb40f5
Osvaldo Torrez <nachi2083@gmail.com>
2022-10-24 23:34:12 -0300

    Enviroment variables and flags added

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
+require('./config/general');
+
 const { spawn } = require('child_process');
 const path = require('path');
 const cron = require('node-cron');
 
-process.env.DB_NAME = process.env.DB_NAME || 'test';
-
-cron.schedule('0 1 * * *', () => backupMongoDB());
+cron.schedule('*/5 * * * * *', () => backupMongoDB());
 
 function backupMongoDB() {
     const ARCHIVE_PATH = path.join(__dirname, 'public', `${process.env.DB_NAME}_${new Date().getTime()}.gzip`);
-    const args = [
-        `--db=${process.env.DB_NAME}`,
-        `--archive=${ARCHIVE_PATH}`,
-        `--gzip`
-    ];
+    let args;
+    if (process.env.NODE_ENV === 'dev') {
+        args = [
+            `--db=${process.env.DB_NAME}`,
+            `--archive=${ARCHIVE_PATH}`,
+            `--gzip`
+        ];
+    } else {
+        args = [
+            `--port ${process.env.DB_PORT}`,
+            `-u ${process.env.USER_NAME}`,
+            `-p ${process.env.USER_PASS}`
+            `--db=${process.env.DB_NAME}`,
+            `--archive=${ARCHIVE_PATH}`,
+            `--gzip`
+        ];
+    }
     
     const child = spawn('mongodump', args);
     child.stdout.on('data', (data) => console.log('stdout: ', data));
---
+++ config/general.js
@@ -0,0 +1,5 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+process.env.DB_NAME = process.env.DB_NAME || 'test';
+process.env.DB_PORT = process.env.DB_PORT || 3000;
+process.env.USER_NAME = process.env.USER_NAME || 'user';
+process.env.USER_PASS = process.env.USER_PASS || 'pass';
